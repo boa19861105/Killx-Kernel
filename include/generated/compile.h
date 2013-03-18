076d3e5bd7a600325184b161176f19d5468a44f5
root <root@ubuntu.(none)>
2013-03-19 04:44:40 +0800

    let's r0x it

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 31337 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#31337 SMP PREEMPT Tue Mar 19 03:45:32 MYT 2013"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.7.2 (Ubuntu/Linaro 4.7.2-1ubuntu1) "
